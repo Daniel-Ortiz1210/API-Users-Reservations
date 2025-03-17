f3793c79f5741195a3019aa15d0c003e5d0c7dcb
Daniel Ortiz <thisisdanielortiz@gmail.com>
2025-03-16 22:36:42 -0600

    Enhance ReservationsRepository with additional fields in select queries and implement delete functionality

--- src/database/repository/reservations.py
+++ src/database/repository/reservations.py
@@ -1,6 +1,8 @@
 from src.database.connection import PostgresqlDatabase
 from src.database.models.reservations import ReservationsModel
 
+from datetime import datetime
+
 from peewee import DoesNotExist
 
 class ReservationsRepository:
@@ -9,7 +11,13 @@ class ReservationsRepository:
         self.model = ReservationsModel
 
     def get_reservations(self):
-        result = self.model.select().dicts()
+        result = self.model.select(
+            self.model.id,
+            self.model.destination,
+            self.model.scheduled_at,
+            self.model.created_at,
+            self.model.updated_at
+        ).dicts()
         return result
 
     def create_reservation(self, reservation: dict):
@@ -19,11 +27,23 @@ class ReservationsRepository:
 
     def get_reservation(self, reservation_id: int):
         try:
-            result = self.model.get_by_id(reservation_id).dicts()
+            result = self.model.select(
+                self.model.id,
+                self.model.destination,
+                self.model.scheduled_at,
+                self.model.created_at,
+                self.model.updated_at
+            ).where(self.model.id == reservation_id).dicts().get()
         except DoesNotExist as e:
             return None
         return result
 
     def update_reservation(self, reservation_id: int, reservation):
-        result = self.model.update(**reservation).where(self.model.id == reservation_id).execute()
+        result = self.model.update(
+            **{"updated_at": datetime.now().isoformat(timespec="minutes"), **reservation}
+        ).where(self.model.id == reservation_id).execute()
+        return result
+    
+    def delete_reservation(self, reservation_id: int):
+        result = self.model.delete().where(self.model.id == reservation_id).execute()
         return result
