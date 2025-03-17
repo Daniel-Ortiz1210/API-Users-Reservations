f3e9818cfce19530c5bc96189a9b81b2fb7843e5
Daniel Ortiz <thisisdanielortiz@gmail.com>
2025-03-16 21:01:05 -0600

    Add ReservationRequestSchema for validating reservation requests

---
+++ src/schemas/requests.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel, field_validator, Field
+from datetime import datetime
+from typing import List, Optional, Dict, Union
+
+
+class ReservationRequestSchema(BaseModel):
+    scheduled_at: str = Field(datetime.now().isoformat(timespec='minutes'), title="Date of reservation", description="The date of the reservation in the format YYYY-MM-DD")
+    destination: str = Field(..., title="Destination", description="The destination of the reservation")
