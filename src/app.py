0bccd49b1c7f5a78fc8536cdd5bf1745653ea4a6
Daniel Ortiz <thisisdanielortiz@gmail.com>
2025-03-16 21:01:18 -0600

    Add startup event to initialize database and create reservation tables

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 
 from src.api.router import router
+from src.database.connection import DatabaseConnection
+from src.database.models.reservations import ReservationsModel
 
 app: FastAPI = FastAPI(
     title='API',
@@ -10,5 +12,10 @@ app: FastAPI = FastAPI(
     redoc_url='/redoc',
     openapi_url='/openapi.json',
 )
+
+@app.on_event('startup')
+async def startup_event():
+    database = DatabaseConnection().get_db()
+    database.create_tables([ReservationsModel])
 
 app.include_router(router)
